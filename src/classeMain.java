de905c45eef67d856b71c12407775bd53f609960
tokyo02 <4hwnBB6Jmluo>
2020-03-16 22:46:50 +0100

    versione precedente

--- src/classeMain.java
+++ src/classeMain.java
@@ -10,8 +10,6 @@ public class classeMain {
         //conta da 1 a 10
         for (int i = 1; i <= 10; i++) {
             System.out.println("numero thread: " + i);
-
-            //fine programma
         }
     }
 }
