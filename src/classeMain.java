d42aab3f0a22538b09454eb45fda6ba4143ade09
tokyo02 <4hwnBB6Jmluo>
2020-03-18 09:18:51 +0100

    Æ

--- src/classeMain.java
+++ src/classeMain.java
@@ -1,15 +1,17 @@
+
 public class classeMain {
     public static void main(String[] args) {
 
         classeThread cos = new classeThread();
         cos.start();
 
-        //nome del thread
+        //nome thread
         System.out.println("Output nome thread: " + Thread.currentThread().getName());
 
-        //conta da 1 a 10
+        //counter da 1 a 10
         for (int i = 1; i <= 10; i++) {
             System.out.println("numero thread: " + i);
+
         }
     }
 }
--- src/classeThread.java
+++ src/classeThread.java
@@ -1,15 +1,15 @@
+
 public class classeThread extends Thread {
     public void run() {
 
-        //nome del thread
+        //nome thread
         setName("classeThread");
         System.out.println("Output nome thread: " + Thread.currentThread().getName());
 
-        //conta da 1 a 10
+        //counter da 1 a 10
         for (int i = 1; i <= 10; i++) {
             System.out.println("numero thread: " + i);
+
         }
     }
-
 }
-
