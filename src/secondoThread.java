200328811dcbec9df2bcf0567dd57e5c4c33e710
Ivan Migliore <migliore.ivan.27@gmail.com>
2020-04-26 17:33:04 +0200

    ae

---
+++ src/secondoThread.java
@@ -0,0 +1,17 @@
+public class secondoThread implements Runnable{
+    public int[] secondiDieci = new int[10];
+    public int sommaSecondo=0;
+    public secondoThread(int[] secondiDieci){
+        this.secondiDieci=secondiDieci;
+    }
+
+    @Override
+    public void run(){
+        System.out.println((Thread.currentThread().getName()+": sommo gli ultimi cinque."));
+        for(int i=0;i<secondiDieci.length;i++){
+            sommaSecondo=sommaSecondo + secondiDieci[i];
+
+        }
+        System.out.println("Tot secondi 5= "+sommaSecondo);
+    }
+}
---
+++ src/terzoThread.java
@@ -0,0 +1,17 @@
+public class terzoThread implements Runnable{
+    public int[] terziDieci = new int[10];
+    public int sommaTerzo=0;
+    public terzoThread(int[] terziDieci){
+        this.terziDieci=terziDieci;
+    }
+
+    @Override
+    public void run(){
+        System.out.println((Thread.currentThread().getName()+": sommo gli ultimi cinque."));
+        for(int i=0;i<terziDieci.length;i++){
+            sommaTerzo=sommaTerzo + terziDieci[i];
+
+        }
+        System.out.println("Tot ultimi 5= "+sommaTerzo);
+    }
+}
